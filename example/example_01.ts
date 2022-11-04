523cfa43bb1d52321af35aff75ac23accaa9e7d5
Vahid V <vhid.vz@gmail.com>
2022-11-04 22:04:55 +0330

    update

--- example/example_01.ts
+++ example/example_01.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
+  ActivityNode,
   DataObject,
   DefineProcess,
   EventNode,
+  GatewayNode,
   Node,
   Param,
   Token,
@@ -25,10 +27,123 @@ class PizzaCustomer extends WorkflowJS {
     console.log(value);
     return { next: node.takeOutgoing() };
   }
+
+  @Node({ name: 'Select a Pizza' })
+  selectAPizza(
+    @Param('node') node: ActivityNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: 'Order a Pizza' })
+  orderAPizza(
+    @Param('node') node: ActivityNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: '«Where Is My Pizza»' })
+  whereIsMyPizza(
+    @Param('node') node: GatewayNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    if (value) return { next: node.$default() };
+    else return { next: node.takeOutgoing({ name: '60 Minutes' }) };
+  }
+
+  @Node({ name: '60 Minutes' })
+  _60minutes(
+    @Param('node') node: EventNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: 'Ask for the pizza' })
+  askForThePizza(
+    @Param('node') node: ActivityNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: 'Pizza Received' })
+  pizzaReceived(
+    @Param('node') node: EventNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: 'Pay the Pizza' })
+  payThePizza(
+    @Param('node') node: ActivityNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ) {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+  }
+
+  @Node({ name: 'Eat the Pizza' })
+  eatThePizza(
+    @Param('node') node: ActivityNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ): DataObject {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+    return { next: node.takeOutgoing() };
+  }
+
+  @Node({ name: 'Hunger Satisfied', end: true })
+  hungerSatisfied(
+    @Param('node') node: EventNode,
+    @Param('token') token: Token,
+    @Param('value') value: any,
+  ) {
+    console.log(node);
+    console.log(token);
+    console.log(value);
+  }
 }
 
 const workflow = new PizzaCustomer();
 
-const token = workflow.run({ node: { name: 'Hungry for Pizza' }, token: new Token() });
+const token = workflow.run({
+  node: { name: 'Hungry for Pizza' },
+  token: new Token(),
+  value: 0,
+});
 
 console.log(token);
+
+console.log(workflow.run({ node: { name: 'Eat the Pizza' }, token }));
